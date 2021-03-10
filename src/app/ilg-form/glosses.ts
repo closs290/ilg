2fdabd640501e1d011c8657655be158c7d6c3f70
closs290 <closs290@regis.edu>
2021-03-09 21:48:00 -0800

    Made clear button prettier, removed unecessary card, and filled out list of languages and gloss abbreviations. Also made gloss hints tooltips instead of unreadable strings wider than gloss width.

--- src/app/ilg-form/glosses.ts
+++ src/app/ilg-form/glosses.ts
@@ -112,4 +112,232 @@ export const LIST_OF_STANDARD_ABBREVIATIONS = [
         abbreviation: 'DEF',
         full: 'definite'
     },
+    {
+        abbreviation: 'DEM',
+        full: 'demonstrative'
+    },
+    {
+        abbreviation: 'DET',
+        full: 'determiner'
+    },
+    {
+        abbreviation: 'DIST',
+        full: 'distal'
+    },
+    {
+        abbreviation: 'DISTR',
+        full: 'distributive'
+    },
+    {
+        abbreviation: 'DU',
+        full: 'dual'
+    },
+    {
+        abbreviation: 'DUR',
+        full: 'durative'
+    },
+    {
+        abbreviation: 'ERG',
+        full: 'ergative'
+    },
+    {
+        abbreviation: 'EXCL',
+        full: 'exclusive'
+    },
+    {
+        abbreviation: 'F',
+        full: 'feminine'
+    },
+    {
+        abbreviation: 'FOC',
+        full: 'focus'
+    },
+    {
+        abbreviation: 'FUT',
+        full: 'future'
+    },
+    {
+        abbreviation: 'GEN',
+        full: 'genitive'
+    },
+    {
+        abbreviation: 'IMP',
+        full: 'imperative'
+    },
+    {
+        abbreviation: 'INCL',
+        full: 'inclusive'
+    },
+    {
+        abbreviation: 'IND',
+        full: 'indicative'
+    },
+    {
+        abbreviation: 'INDF',
+        full: 'indefinite'
+    },
+    {
+        abbreviation: 'INF',
+        full: 'infinitive'
+    },
+    {
+        abbreviation: 'INS',
+        full: 'instrumental'
+    },
+    {
+        abbreviation: 'INTR',
+        full: 'intransitive'
+    },
+    {
+        abbreviation: 'IPFV',
+        full: 'imperfective'
+    },
+    {
+        abbreviation: 'IRR',
+        full: 'irrealis'
+    },
+    {
+        abbreviation: 'LOC',
+        full: 'locative'
+    },
+    {
+        abbreviation: 'M',
+        full: 'masculine'
+    },
+    {
+        abbreviation: 'N',
+        full: 'neuter'
+    },
+    {
+        abbreviation: 'N-',
+        full: 'non-'
+    },
+    {
+        abbreviation: 'NEG',
+        full: 'negative'
+    },
+    {
+        abbreviation: 'NMLZ',
+        full: 'nominalizer'
+    },
+    {
+        abbreviation: 'NOM',
+        full: 'nominative'
+    },
+    {
+        abbreviation: 'OBJ',
+        full: 'object'
+    },
+    {
+        abbreviation: 'OBL',
+        full: 'oblique'
+    },
+    {
+        abbreviation: 'P',
+        full: 'patient'
+    },
+    {
+        abbreviation: 'PASS',
+        full: 'passive'
+    },
+    {
+        abbreviation: 'PFV',
+        full: 'perfective'
+    },
+    {
+        abbreviation: 'PL',
+        full: 'plural'
+    },
+    {
+        abbreviation: 'POSS',
+        full: 'possessive'
+    },
+    {
+        abbreviation: 'PRED',
+        full: 'predicative'
+    },
+    {
+        abbreviation: 'PRF',
+        full: 'perfect'
+    },
+    {
+        abbreviation: 'PRS',
+        full: 'present'
+    },
+    {
+        abbreviation: 'PROG',
+        full: 'progressive'
+    },
+    {
+        abbreviation: 'PROH',
+        full: 'prohibitive'
+    },
+    {
+        abbreviation: 'PROX',
+        full: 'proximal'
+    },
+    {
+        abbreviation: 'PST',
+        full: 'past'
+    },
+    {
+        abbreviation: 'PTCP',
+        full: 'participle'
+    },
+    {
+        abbreviation: 'PURP',
+        full: 'purposive'
+    },
+    {
+        abbreviation: 'Q',
+        full: 'question particle'
+    }, 
+    {
+        abbreviation: 'QUOT',
+        full: 'quotative'
+    },
+    {
+        abbreviation: 'RECP',
+        full: 'reciprocal'
+    },
+    {
+        abbreviation: 'REFL',
+        full: 'reflexive'
+    },
+    {
+        abbreviation: 'REL',
+        full: 'relative'
+    },
+    {
+        abbreviation: 'RES',
+        full: 'resultative'
+    },
+    {
+        abbreviation: 'S',
+        full: 'single argument'
+    },
+    {
+        abbreviation: 'SBJ',
+        full: 'subject'
+    },
+    {
+        abbreviation: 'SBJV',
+        full: 'subjunctive'
+    },
+    {
+        abbreviation: 'SG',
+        full: 'singular'
+    },
+    {
+        abbreviation: 'TOP',
+        full: 'topic'
+    },
+    {
+        abbreviation: 'TR',
+        full: 'transitive'
+    },
+    {
+        abbreviation: 'VOC',
+        full: 'vocative'
+    }
 ];
