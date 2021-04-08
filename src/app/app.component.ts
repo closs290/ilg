4e9ddf2108f3fd7c2774344cea6ab07836510e64
closs290 <closs290@regis.edu>
2021-04-07 21:31:10 -0700

    Changed favicon and title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ilg';
+  title = 'Interlinear Gloss Creator Web App';
 }
