d6d6515971fe0af8399226f62b29995177905474
closs290 <closs290@regis.edu>
2021-04-28 18:21:38 -0700

    Nonworking commit just to back things up before an experiment.

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,10 @@
-import express = require('express');
-import bodyParser = require('body-parser');
-import cors = require('cors');
+import * as mongoose from 'mongoose';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+
+const databaseName = 'mongodb://localhost:27017/characters'; 
+// might need to be: mongodb://localhost:27017/interlinear-gloss-library
 
 const app = express();
 
@@ -17,3 +21,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set port number
 const port = process.env.PORT || 3000;
+
+// Connecting to database
+mongoose.connect(databaseName, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+
+// If there is a connection error with db
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// If DB is opened successfully
+db.once('open', () => {
+  console.log('Connection Successful!');
+});
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+const ilgRoutes = []; // These needs updated before anything will work
+
+app.use('/interlinear-gloss-library', ilgRoutes);
+
+// Start Server
+app.listen(port, () => {
+  console.log('Server started and listening on port ' + port);
+});
--- backend/ilg.model.ts
+++ backend/ilg.model.ts
@@ -1,21 +1,30 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema, Document } from 'mongoose';
 
-//module.exports makes me nervous
-module.exports = mongoose => {
-    const ILG = mongoose.model(
-        "ilg",
-        mongoose.Schema(
-            {
-                language: String, 
-                author: String,
-                year: String,
-                phrases: [],
-                freeTranslation: String
-            }
-        )
-    );
-    return ILG;
+export interface interlinearGloss extends Document {
+    language: string;
+    author: string;
+    year: string;
+    phrases: [];
+    freeTranslation: string;
 }
+
+export const ILGSchema = new Schema<interlinearGloss>({
+    language: {
+        type: String
+    }, 
+    author: {
+        type: String
+    },
+    year: {
+        type: String
+    }, 
+    phrases: {
+        type: Array
+    }, 
+    freeTranslation: {
+        type: String
+    }
+});
 
 export interface phrase {
     orthography?: string;
@@ -23,3 +32,5 @@ export interface phrase {
     gloss: string;
     abbreviation?: string
 }
+
+export const ILGs = model('ILG', ILGSchema);
